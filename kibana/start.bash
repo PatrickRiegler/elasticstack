0c309c4bbee0c5d87d2995ee1f40c784a2066fa0
tkr6q (tkr6q) <patrick.riegler@six-group.com>
2017-11-01 16:07:17 +0100

    DEVTS-6994 and DEVTS-7011 - patch the hostname of kibana

--- kibana/start.bash
+++ kibana/start.bash
@@ -3,5 +3,8 @@
 if [ "${ELASTICSEARCH_URL}" != "" ]; then
   sed -ri "s,#elasticsearch.url:.*,elasticsearch.url: ${ELASTICSEARCH_URL}," ${PROD_INSTALL}/config/kibana.yml
 fi
+if [ "${SERVER_HOST}" != "" ]; then
+  sed -ri "s,#server.host:.*,server.host: ${SERVER_HOST}," ${PROD_INSTALL}/config/kibana.yml
+fi
 
 ${PROD_INSTALL}/bin/kibana ${STARTUP_PARAMS}
