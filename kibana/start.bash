00072354d86e86ffe49140b8f1bf5742a43c4a5f
tkr6q (tkr6q) <patrick.riegler@six-group.com>
2017-11-01 16:20:15 +0100

    DEVTS-6994 and DEVTS-7011 - fix patching mechanism to automatically assign IP address

--- kibana/start.bash
+++ kibana/start.bash
@@ -5,6 +5,8 @@ if [ "${ELASTICSEARCH_URL}" != "" ]; then
 fi
 if [ "${SERVER_HOST}" != "" ]; then
   sed -ri "s,#server.host:.*,server.host: ${SERVER_HOST}," ${PROD_INSTALL}/config/kibana.yml
+else
+  sed -ri "s,#server.host:.*,server.host: $(getent ahostsv4 $(hostname) | awk '{ print $1 }' | head -n 1)," ${PROD_INSTALL}/config/kibana.yml
 fi
 
 ${PROD_INSTALL}/bin/kibana ${STARTUP_PARAMS}
