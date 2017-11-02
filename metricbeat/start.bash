abfd8052775394c37a2918beca5417cec36037a7
tkr6q (tkr6q) <patrick.riegler@six-group.com>
2017-11-02 15:44:13 +0100

    DEVTS-6994 and DEVTS-7015 - patch configration of metricbeat

--- metricbeat/start.bash
+++ metricbeat/start.bash
@@ -1,4 +1,12 @@
 #!/usr/bin/dumb-init /bin/bash
 
+if [ -z "${CONF}" ]; then
+	export CONF="metricbeat.yml"
+fi
+
+if [ ! -z "${ELASTICSEARCH_URL}" ]; then
+	sed -i "s,localhost:9200,${ELASTICSEARCH_URL}," ${PROD_INSTALL}/${CONF}
+fi
+
 ${PROD_INSTALL}/metricbeat -e -c ${PROD_INSTALL}/${CONF} -d "publish"
 
