5d2e2fcb8593c7e884ca13f7d9c15ee8ad182f0f
tkr6q (tkr6q) <patrick.riegler@six-group.com>
2017-11-02 12:49:43 +0100

    DEVTS-6994 and DEVTS-7012 - fix patching in filebeat image

--- filebeat/start.bash
+++ filebeat/start.bash
@@ -9,5 +9,5 @@ if [ ! -z "${ELASTICSEARCH_URL}" ]; then
 	sed -i "s,localhost:9200,${ELASTICSEARCH_URL}," ${PROD_INSTALL}/${CONF}
 fi
 
-${PROD_INSTALL}/filebeat -f ${PROD_INSTALL}/${CONF}
+${PROD_INSTALL}/filebeat 
 
