4163fb88fe9dc0e6823aba4714450b377cb50b2c
tkr6q (tkr6q) <patrick.riegler@six-group.com>
2017-11-02 13:53:22 +0100

    DEVTS-6994 and DEVTS-7012 - build autologger for filebeat

--- filebeat/start.bash
+++ filebeat/start.bash
@@ -9,5 +9,10 @@ if [ ! -z "${ELASTICSEARCH_URL}" ]; then
 	sed -i "s,localhost:9200,${ELASTICSEARCH_URL}," ${PROD_INSTALL}/${CONF}
 fi
 
+if [ "$AUTOLOGGER" = "true" ]; then
+sed -ri "s,/var/log/.*,$PROD_INSTALL/request.log," $PROD_INSTALL/${CONF}
+(while true; do  echo "$(date +"%Y%m%d%H%M%S")|4|REQUEST|10.16.5.206|tkr6q|GET|/get/$(date +"%Y%m%d")/$(echo $((RANDOM%20+10)))|HTTP/1.1|200|0" >> ${PROD_INSTALL}/request.log; sleep ${SLEEP}; done) &
+fi
+
 ${PROD_INSTALL}/filebeat 
 
