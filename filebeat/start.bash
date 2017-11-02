a88039e2ec8d13d1f4b8e57e44c9427a1d95207a
tkr6q (tkr6q) <patrick.riegler@six-group.com>
2017-11-02 12:42:57 +0100

    DEVTS-6994 and DEVTS-7012 - filebeat fix patching in start.bash

--- filebeat/start.bash
+++ filebeat/start.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/dumb-init /bin/bash
 
 if [ -z "${CONF}" ]; then
-	export CONF="config/filebeat.conf"
+	export CONF="filebeat.yml"
 fi
 
 #sed -i "s,PROD_INSTALL,${PROD_INSTALL},g" ${PROD_INSTALL}/${CONF}
 if [ ! -z "${ELASTICSEARCH_URL}" ]; then
-	sed -i "s,ELASTICSEARCH_URL,${ELASTICSEARCH_URL}," ${PROD_INSTALL}/${CONF}
+	sed -i "s,localhost:9200,${ELASTICSEARCH_URL}," ${PROD_INSTALL}/${CONF}
 fi
 
-${PROD_INSTALL}/bin/filebeat -f ${PROD_INSTALL}/${CONF}
+${PROD_INSTALL}/filebeat -f ${PROD_INSTALL}/${CONF}
 
