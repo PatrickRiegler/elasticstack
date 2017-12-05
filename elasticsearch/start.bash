3aea00474b419921c522eabb72262721a91e9d5a
tkr6q (tkr6q) <patrick.riegler@six-group.com>
2017-12-05 09:08:43 +0100

    DEVTS-6995 and DEVTS-7463 - fix the java memory issue with using the feature in the base image

--- elasticsearch/start.bash
+++ elasticsearch/start.bash
@@ -5,5 +5,5 @@ sed -ri "s,network.host:.*,network.host: $(getent ahostsv4 $(hostname) | awk '{ 
 sed -ri "s,#network.host:.*,network.host: $(getent ahostsv4 $(hostname) | awk '{ print $1 }' | head -n 1)," ${PROD_INSTALL}/config/elasticsearch.yml
 sed -ri "s,#network.host: 192.168.0.1,network.host: $(getent ahostsv4 $(hostname) | awk '{ print $1 }' | head -n 1)," ${PROD_INSTALL}/config/elasticsearch.yml
 
-export ES_JAVA_OPTS="$ES_JAVA_OPTS $(/opt/run-java/java-default-options)"
+export ES_JAVA_OPTS="$ES_JAVA_OPTS -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
 ${PROD_INSTALL}/bin/elasticsearch ${STARTUP_PARAMS} -Enode.name=$(hostname)
